c14675cff5a5ab364e0b7d01b26e340011d0c38e
FrancoBugnaaa <101563222+FrancoBugnaaa@users.noreply.github.com>
2022-05-20 14:27:31 -0300

    Ej prefab4 DOne

---
+++ Assets/scripts/Prefab_Cloner.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Prefab_Cloner : MonoBehaviour
+{
+    public GameObject Prefab;
+    public Text inputCLoneAmount;
+    public Text displayClonesLeft;
+    public InputField Ifield;
+    int counter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        counter = 0;
+        displayClonesLeft.text = "?";
+    }
+
+    public void Clone_Prefab()
+    {
+
+        if(inputCLoneAmount.text!= "")
+        {
+            //esto es para que si esta vacio no se active ni siquiera.
+            Ifield.readOnly = true;
+
+            int cloneAmount = int.Parse(inputCLoneAmount.text);
+
+            if (counter < cloneAmount)
+            {
+                Instantiate(Prefab);
+                counter++;
+                displayClonesLeft.text = (cloneAmount - counter).ToString();
+            }
+            else
+            {
+                displayClonesLeft.text = "Te pasaste";
+            }
+        }
+        else
+        {
+            displayClonesLeft.text = "ERROR";
+        }
+  
+    }
+}
